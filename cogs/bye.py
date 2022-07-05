f10affffdf7d9d6f16427d305d4cab10d53b55d7
Java sauce <mohammedbrandon204@gmail.com>
2022-07-05 19:54:51 -0400

    Update bye.py

--- cogs/bye.py
+++ cogs/bye.py
@@ -2,7 +2,7 @@ import discord.py
 from discord.ext import commands
 
 
-Class Welcomer(commands.cog)
+Class Bye(commands.cog)
     def __init__(self,client)
     self.client = client
 
@@ -11,4 +11,4 @@ Class Welcomer(commands.cog)
     async def on_member_leave(self, ctx,member):
     guild =  client.get_guild(844199297523646505)
     channel = guild.get_channel(863605206301671454)
-    await channel.send(f":tada: Somebody just left the sever")
+    await channel.send(f":tada: {member.mention just left the sever")
