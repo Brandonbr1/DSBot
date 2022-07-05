24faa75f64ff536ff418901287f80f1897567b6f
Java sauce <mohammedbrandon204@gmail.com>
2022-07-05 19:47:53 -0400

    add code for the Welcomer

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -8,6 +8,10 @@ Class Welcomer(commands.cog)
 
 
     @commands.cog.listener()
+    async def on_member_join(member):
+    guild =  client.get_guild(844199297523646505)
+    channel = guild.get_channel(863605206301671454)
+    await channel.send(f":tada: Welcome to the sever {member.mention} you joining our discord sever means so mutch to us")
     
 
 
