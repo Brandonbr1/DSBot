32a7a651eb5593b6a06eeca45760b38ed9e2c63e
Java sauce <mohammedbrandon204@gmail.com>
2022-07-05 19:45:14 -0400

    basic cog test

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,1 +1,16 @@
+import discord.py
+from discord.ext import commands
+
+
+Class Welcomer(commands.cog)
+    def __init__(self,client)
+    self.client = client
+
+
+    @commands.cog.listener()
+    
+
+
+
+
 
